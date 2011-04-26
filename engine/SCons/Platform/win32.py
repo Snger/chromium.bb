c81c95aff36392cffede13382dd5fcad25c0a603
bradnelson@google.com <bradnelson@google.com@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2011-04-26 00:55:24 +0000

    Applying this fix:
    http://scons.tigris.org/issues/show_bug.cgi?id=2449
    
    Which we had done in the previous scons version.
    
    BUG=None
    TEST=None
    R=noelallen@google.com
    Review URL: http://codereview.chromium.org/6880195
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/third_party/scons-2.0.1@5025 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2

--- engine/SCons/Platform/win32.py
+++ engine/SCons/Platform/win32.py
@@ -41,6 +41,36 @@ from SCons.Platform.posix import exitvalmap
 from SCons.Platform import TempFileMunge
 import SCons.Util
 
+#NACL_CHANGE(bradnelson) - FROM THIS:
+#NACL_CHANGE(bradnelson) - TO THIS:
+# NOTE: Both python and SCons are NOT planning to fix this, so we will need this
+# fix even when upgrading to newer versions of scons or python.
+
+import threading
+
+# Monkey patch os.spawnve on Windows to become thread safe
+from os import spawnve as old_spawnve
+
+spawn_lock = threading.Lock()
+
+def new_spawnve(mode, file, args, env):
+    spawn_lock.acquire()
+    try:
+        if mode == os.P_WAIT:
+            ret = old_spawnve(os.P_NOWAIT, file, args, env)
+        else:
+            ret = old_spawnve(mode, file, args, env)
+    finally:
+        spawn_lock.release()
+    if mode == os.P_WAIT:
+        pid, status = os.waitpid(ret, 0)
+        ret = status >> 8
+    return ret
+
+os.spawnve = new_spawnve
+
+#NACL_CHANGE(bradnelson) - END CHANGES.
+
 try:
     import msvcrt
     import win32api
