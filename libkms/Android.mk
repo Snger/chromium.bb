220d0771e51a2f978c93a55958ea6223f78ac590
Nicolas Boichat <drinkcat@google.com>
2015-12-14 17:11:20 +0800

    CHROMIUM: mediatek: Add support for Android build system
    This patch adds changes necessary to enable building libdrm_mediatek
    inside Android build system.
    
    BUG=b:26864637
    TEST=N/A
    
    Change-Id: I0788fa4c1eee1a8c5fde2d32890493c5f475b5d5
    Signed-off-by: Nicolas Boichat <drinkcat@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/324676
    Commit-Ready: Nicolas Boichat <drinkcat@chromium.org>
    Tested-by: Nicolas Boichat <drinkcat@chromium.org>
    Reviewed-by: Nicolas Boichat <drinkcat@chromium.org>

--- libkms/Android.mk
+++ libkms/Android.mk
@@ -3,6 +3,7 @@ DRM_GPU_DRIVERS := $(strip $(filter-out swrast, $(BOARD_GPU_DRIVERS)))
 intel_drivers := i915 i965 i915g ilo
 radeon_drivers := r300g r600g radeonsi
 rockchip_drivers := rockchip
+mediatek_drivers := mediatek
 nouveau_drivers := nouveau
 virgl_drivers := virgl
 vmwgfx_drivers := vmwgfx
@@ -11,6 +12,7 @@ valid_drivers := \
 	$(intel_drivers) \
 	$(radeon_drivers) \
 	$(rockchip_drivers) \
+	$(mediatek_drivers) \
 	$(nouveau_drivers) \
 	$(virgl_drivers) \
 	$(vmwgfx_drivers)
