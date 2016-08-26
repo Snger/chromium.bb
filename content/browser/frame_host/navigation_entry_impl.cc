Snger/chromium.bb

commit b89c5716318ac86665bba490c121e942bd76658e

Ensure FrameNavigationEntry is fully updated.
Currently RendererDidNavigateToExistingPage and RendererDidNavigateToSamePage
only partially update the FrameNavigationEntry for the committing
navigation. This leads to inconsistency between sequence numbers and
PageState, which can cause origin/URL mismatches.

This CL updates both these methods to call AddOrUpdateFrameEntry to
ensure that all members are properly updated.

BUG=630103, 628677
CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_site_isolation

Review-Url: https://codereview.chromium.org/2224213005
Cr-Commit-Position: refs/heads/master@{#411224}
(cherry picked from commit 6bcea1663ac7a6ac101674a7f4b98f366c21d557 [formerly af18219268a332a92b3c8bbaa306aade92142c55])
TBR=creis@chromium.org

Review URL: https://codereview.chromium.org/2284533002 .

Cr-Commit-Position: refs/branch-heads/2785@{#755}
Cr-Branched-From: 1ac8cbeb262a801827381e10bb796676610ddf00 [formerly 68623971be0cfc492a2cb0427d7f478e7b214c24]-refs/heads/master@{#403382}

Former-commit-id: 4ab88c96e291c275b3c0801aaffe13f124dd427d

content/browser/frame_host/navigation_entry_impl.cc
@@ -697,9 +697,33 @@ void NavigationEntryImpl::AddOrUpdateFrameEntry(
     const PageState& page_state,
     const std::string& method,
     int64_t post_id) {
+  // We should only have an empty PageState if the navigation is new, and thus
+  // page ID is -1.
+  if (!page_state.IsValid() && GetPageID() != -1) {
+    // Temporarily generate a minidump to diagnose https://crbug.com/568703.
+    base::debug::DumpWithoutCrashing();
+    NOTREACHED() << "Shouldn't set an empty PageState.";
+  }
+
+  // If this is called for the main frame, the FrameNavigationEntry is
+  // guaranteed to exist, so just update it directly and return.
+  if (frame_tree_node->IsMainFrame()) {
+    // If the document of the FrameNavigationEntry is changing, we must clear
+    // any child FrameNavigationEntries.
+    if (root_node()->frame_entry->document_sequence_number() !=
+        document_sequence_number)
+      root_node()->children.clear();
+
+    root_node()->frame_entry->UpdateEntry(
+        frame_tree_node->unique_name(), item_sequence_number,
+        document_sequence_number, site_instance,
+        std::move(source_site_instance), url, referrer, page_state, method,
+        post_id);
+    return;
+  }
+
   // We should already have a TreeNode for the parent node by the time this node
   // commits.  Find it first.
-  DCHECK(frame_tree_node->parent());
   NavigationEntryImpl::TreeNode* parent_node =
       FindFrameEntry(frame_tree_node->parent());
   if (!parent_node) {

content/browser/frame_host/navigation_controller_impl.cc
@@ -1150,6 +1150,9 @@ void NavigationControllerImpl::RendererDidNavigateToExistingPage(
   // We should only get here for main frame navigations.
   DCHECK(!rfh->GetParent());
 
+  // TODO(creis): Classify location.replace as NEW_PAGE instead of EXISTING_PAGE
+  // in https://crbug.com/596707.
+
   NavigationEntryImpl* entry;
   if (params.intended_as_new_entry) {
     // This was intended as a new entry but the pending entry was lost in the
@@ -1175,30 +1178,23 @@ void NavigationControllerImpl::RendererDidNavigateToExistingPage(
   if (entry->update_virtual_url_with_url())
     UpdateVirtualURLToURL(entry, params.url);
 
-  // Update the post parameters.
-  FrameNavigationEntry* frame_entry =
-      entry->GetFrameEntry(rfh->frame_tree_node());
-  frame_entry->set_method(params.method);
-  frame_entry->set_post_id(params.post_id);
+  // The site instance will normally be the same except during session restore,
+  // when no site instance will be assigned.
+  DCHECK(entry->site_instance() == nullptr ||
+         entry->site_instance() == rfh->GetSiteInstance());
 
-  // Update the ISN and DSN in case this was a location.replace, which can cause
-  // them to change.
-  // TODO(creis): Classify location.replace as NEW_PAGE instead of EXISTING_PAGE
-  // in https://crbug.com/596707.
-  frame_entry->set_item_sequence_number(params.item_sequence_number);
-  frame_entry->set_document_sequence_number(params.document_sequence_number);
+  // Update the existing FrameNavigationEntry to ensure all of its members
+  // reflect the parameters coming from the renderer process.
+  entry->AddOrUpdateFrameEntry(
+      rfh->frame_tree_node(), params.item_sequence_number,
+      params.document_sequence_number, rfh->GetSiteInstance(), nullptr,
+      params.url, params.referrer, params.page_state, params.method,
+      params.post_id);
 
   // The redirected to page should not inherit the favicon from the previous
   // page.
   if (ui::PageTransitionIsRedirect(params.transition))
     entry->GetFavicon() = FaviconStatus();
-
-  // The site instance will normally be the same except during session restore,
-  // when no site instance will be assigned.
-  DCHECK(entry->site_instance() == nullptr ||
-         entry->site_instance() == rfh->GetSiteInstance());
-  entry->set_site_instance(
-      static_cast<SiteInstanceImpl*>(rfh->GetSiteInstance()));
 
   // The entry we found in the list might be pending if the user hit
   // back/forward/reload. This load should commit it (since it's already in the
@@ -1239,13 +1235,14 @@ void NavigationControllerImpl::RendererDidNavigateToSamePage(
   if (existing_entry->update_virtual_url_with_url())
     UpdateVirtualURLToURL(existing_entry, params.url);
   existing_entry->SetURL(params.url);
-  existing_entry->SetReferrer(params.referrer);
 
-  // The page may have been requested with a different HTTP method.
-  FrameNavigationEntry* frame_entry =
-      existing_entry->GetFrameEntry(rfh->frame_tree_node());
-  frame_entry->set_method(params.method);
-  frame_entry->set_post_id(params.post_id);
+  // Update the existing FrameNavigationEntry to ensure all of its members
+  // reflect the parameters coming from the renderer process.
+  existing_entry->AddOrUpdateFrameEntry(
+      rfh->frame_tree_node(), params.item_sequence_number,
+      params.document_sequence_number, rfh->GetSiteInstance(), nullptr,
+      params.url, params.referrer, params.page_state, params.method,
+      params.post_id);
 
   DiscardNonCommittedEntries();
 }

content/browser/frame_host/navigation_controller_impl_browsertest.cc
@@ -4928,4 +4928,64 @@ IN_PROC_BROWSER_TEST_F(NavigationControllerBrowserTest, PostViaOpenUrlMsg) {
   EXPECT_EQ("text=value\n", body);
 }
 
+// Test that navigations classified as SAME_PAGE properly update all the
+// members of FrameNavigationEntry. If not, it is possible to get a mismatch
+// between the origin and URL of a document as seen in
+// https://crbug.com/630103.
+IN_PROC_BROWSER_TEST_F(NavigationControllerBrowserTest,
+                       EnsureSamePageNavigationUpdatesFrameNavigationEntry) {
+  WebContentsImpl* web_contents =
+      static_cast<WebContentsImpl*>(shell()->web_contents());
+  FrameTreeNode* root = web_contents->GetFrameTree()->root();
+
+  // Navigate to a simple page and then perform an in-page navigation.
+  GURL start_url(embedded_test_server()->GetURL("a.com", "/title1.html"));
+  EXPECT_TRUE(NavigateToURL(shell(), start_url));
+
+  GURL same_page_url(
+      embedded_test_server()->GetURL("a.com", "/title1.html#foo"));
+  EXPECT_TRUE(NavigateToURL(shell(), same_page_url));
+  EXPECT_EQ(2, web_contents->GetController().GetEntryCount());
+
+  // Replace the URL of the current NavigationEntry with one that will cause
+  // a server redirect when loaded.
+  {
+    GURL redirect_dest_url(
+        embedded_test_server()->GetURL("sub.a.com", "/simple_page.html"));
+    TestNavigationObserver observer(web_contents);
+    std::string script = "history.replaceState({}, '', '/server-redirect?" +
+                         redirect_dest_url.spec() + "')";
+    EXPECT_TRUE(ExecuteScript(root, script));
+    observer.Wait();
+  }
+
+  // Simulate the user hitting Enter in the omnibox without changing the URL.
+  {
+    TestNavigationObserver observer(web_contents);
+    web_contents->GetController().LoadURL(web_contents->GetLastCommittedURL(),
+                                          Referrer(), ui::PAGE_TRANSITION_LINK,
+                                          std::string());
+    observer.Wait();
+  }
+
+  // Prior to fixing the issue, the above omnibox navigation (which is
+  // classified as SAME_PAGE) was leaving the FrameNavigationEntry with the
+  // same document sequence number as the previous entry but updates the URL.
+  // Doing a back session history navigation now will cause the browser to
+  // consider it as in-page because of this matching document sequence number
+  // and lead to a mismatch of origin and URL in the renderer process.
+  {
+    TestNavigationObserver observer(web_contents);
+    web_contents->GetController().GoBack();
+    observer.Wait();
+  }
+
+  // Verify the expected origin through JavaScript. It also has the additional
+  // verification of the process also being still alive.
+  std::string origin;
+  EXPECT_TRUE(ExecuteScriptAndExtractString(
+      web_contents, "domAutomationController.send(document.origin)", &origin));
+  EXPECT_EQ(start_url.GetOrigin().spec(), origin + "/");
+}
+
 }  // namespace content

testing/buildbot/filters/browser-side-navigation.linux.content_browsertests.filter
@@ -2,6 +2,7 @@
 -IFrameZoomBrowserTest.RedirectToPageWithSubframeZoomsCorrectly
 -IFrameZoomBrowserTest.SubframesDontZoomIndependently
 -NavigationControllerBrowserTest.BackTwiceToIframeWithContent
+-NavigationControllerBrowserTest.EnsureSamePageNavigationUpdatesFrameNavigationEntry
 -RenderFrameHostManagerTest.RestoreSubframeFileAccessForHistoryNavigation
 -RequestDataResourceDispatcherHostBrowserTest.*
 -ServiceWorker*

commit 9fde757cc09dbe143b340a9b028a414711df97b7

Remove extraneous base::debug::DumpWithoutCrashing call.
This is needed to fix the compile error introduced by b89c5716318ac86665bba490c121e942bd76658e [formerly 4ab88c96e291c275b3c0801aaffe13f124dd427d].

BUG=641233
CQ_INCLUDE_TRYBOTS=tryserver.chromium.linux:linux_site_isolation
TBR=creis@chromium.org

Review URL: https://codereview.chromium.org/2285653002 .

Cr-Commit-Position: refs/branch-heads/2785@{#756}
Cr-Branched-From: 1ac8cbeb262a801827381e10bb796676610ddf00 [formerly 68623971be0cfc492a2cb0427d7f478e7b214c24]-refs/heads/master@{#403382}

Former-commit-id: 59fc95f688accb33883ef6fb8ea75cf23b8aaa1e

content/browser/frame_host/navigation_entry_impl.cc
@@ -697,14 +697,6 @@ void NavigationEntryImpl::AddOrUpdateFrameEntry(
     const PageState& page_state,
     const std::string& method,
     int64_t post_id) {
-  // We should only have an empty PageState if the navigation is new, and thus
-  // page ID is -1.
-  if (!page_state.IsValid() && GetPageID() != -1) {
-    // Temporarily generate a minidump to diagnose https://crbug.com/568703.
-    base::debug::DumpWithoutCrashing();
-    NOTREACHED() << "Shouldn't set an empty PageState.";
-  }
-
   // If this is called for the main frame, the FrameNavigationEntry is
   // guaranteed to exist, so just update it directly and return.
   if (frame_tree_node->IsMainFrame()) {