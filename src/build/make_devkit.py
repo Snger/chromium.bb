48334f09753bb84515d3340951f3e568d7a9c4b9
Imran Haider <ihaider3@bloomberg.net>
2018-06-20 17:19:54 -0400

    Update devkit version bb709

--- src/build/make_devkit.py
+++ src/build/make_devkit.py
@@ -186,7 +186,7 @@ def addGenFiles():
 
 
 def main(args):
-  version = "bb708"   # SET BB VERSION NUMBER HERE
+  version = "bb709"   # SET BB VERSION NUMBER HERE
 
   outDir = None
   doClean = True
