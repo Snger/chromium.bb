25d32f28a0d866fe126debd808d7c05c863c7059
Zhen Yin <zyin@bloomberg.net>
2018-06-25 11:18:10 -0400

    bb62/feature/listMarkerEnhancements

--- src/third_party/WebKit/Source/core/layout/LayoutBlock.cpp
+++ src/third_party/WebKit/Source/core/layout/LayoutBlock.cpp
@@ -557,6 +557,18 @@ void LayoutBlock::AddVisualOverflowFromTheme() {
   AddSelfVisualOverflow(LayoutRect(inflated_rect));
 }
 
+LayoutUnit LayoutBlock::additionalMarginStart() const
+{
+    if (IsInline() || !Parent() || Parent()->ChildrenInline()
+          || !Parent()->GetNode() || (!Parent()->GetNode()->HasTagName(HTMLNames::ulTag) &&
+        !Parent()->GetNode()->HasTagName(HTMLNames::olTag))) {
+        return LayoutUnit(0);
+    }
+
+    LayoutBox *previousBox = PreviousSiblingBox();
+    return previousBox ? previousBox->additionalMarginStart() : LayoutUnit(40);
+}
+
 static inline bool ChangeInAvailableLogicalHeightAffectsChild(
     LayoutBlock* parent,
     LayoutBox& child) {
@@ -1484,6 +1496,11 @@ void LayoutBlock::ComputeBlockPreferredLogicalWidths(
       margin_start += start_margin_length.Value();
     if (end_margin_length.IsFixed())
       margin_end += end_margin_length.Value();
+
+    // SHEZ: additionalMarginStart is treated as fixed margin
+    if (child->IsBox())
+        margin_start += ToLayoutBox(child)->additionalMarginStart();      
+
     margin = margin_start + margin_end;
 
     LayoutUnit child_min_preferred_logical_width,
