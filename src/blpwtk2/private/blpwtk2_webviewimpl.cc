38250944a08e2d248e00eb965a56855a2bb3dbf0
yren93 <yren93@bloomberg.net>
2018-07-11 13:03:21 -0400

    Bb62/blpwtk2/screen print (#57)
    * support screen-print
    
    * Formatting and make screen-print buildable
    
    * Screen-print, use unordered_map to keep track of temporarily updated document body styles
    
    * fix typo

--- src/blpwtk2/private/blpwtk2_webviewimpl.cc
+++ src/blpwtk2/private/blpwtk2_webviewimpl.cc
@@ -256,6 +256,11 @@ int WebViewImpl::loadUrl(const StringRef& url)
     return 0;
 }
 
+void WebViewImpl::drawContentsToBlob(Blob *blob, const DrawParams& params)
+{
+    NOTREACHED() << "drawContentsToBlob() not supported in WebViewImpl";
+}
+
 int WebViewImpl::getRoutingId() const
 {
     DCHECK(Statics::isInBrowserMainThread());
