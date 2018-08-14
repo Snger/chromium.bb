74448682fc854933a5d7d898f9a8bd1ed6a5cfa8
Imran Haider <ihaider3@bloomberg.net>
2018-08-14 11:28:36 -0400

    Update devkit version to bb810

--- src/build/make_devkit.py
+++ src/build/make_devkit.py
@@ -187,7 +187,7 @@ def addGenFiles():
 
 
 def main(args):
-  version = "bb809"   # SET BB VERSION NUMBER HERE
+  version = "bb810"   # SET BB VERSION NUMBER HERE
 
   outDir = None
   doClean = True
