7bb1601a662208ae5a4609402fb8ce793fbf91fe
Alex Betts <abetts3@bloomberg.net>
2018-08-24 16:37:41 -0400

    Update devkit version to bb813

--- src/build/make_devkit.py
+++ src/build/make_devkit.py
@@ -187,7 +187,7 @@ def addGenFiles():
 
 
 def main(args):
-  version = "bb812"   # SET BB VERSION NUMBER HERE
+  version = "bb813"   # SET BB VERSION NUMBER HERE
 
   outDir = None
   doClean = True
