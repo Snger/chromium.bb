ab5d2ed3327393320bd9b4722ed207ea8959e5ed
Inseob Lee <ilee56@bloomberg.net>
2018-09-27 09:46:57 -0400

    Allow embedder to notify blpwtk2 when DWM mode is toggled
    Rebased from commit 451c7a119747 of Chromium 67

--- src/blpwtk2/private/blpwtk2_webviewhostimpl.h
+++ src/blpwtk2/private/blpwtk2_webviewhostimpl.h
@@ -150,6 +150,7 @@ class WebViewHostImpl final : private WebViewImplClient
                      applyRegionCallback callback) override;
     void clearTooltip() override;
     void setParent(unsigned int window) override;
+    void rootWindowCompositionChanged() override;
 
     DISALLOW_COPY_AND_ASSIGN(WebViewHostImpl);
 
