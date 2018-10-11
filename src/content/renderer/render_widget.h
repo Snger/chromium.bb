dda68b84b05d56658121b00b7b41e01a1e2e8d53
Imran Haider <ihaider3@bloomberg.net>
2018-10-11 11:50:17 -0400

    fix build

--- src/content/renderer/render_widget.h
+++ src/content/renderer/render_widget.h
@@ -998,9 +998,6 @@ class CONTENT_EXPORT RenderWidget
   base::WeakPtrFactory<RenderWidget> weak_ptr_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(RenderWidget);
-
- private:
-  bool bb_OnHandleInputEvent_no_ack_;
 };
 
 }  // namespace content
