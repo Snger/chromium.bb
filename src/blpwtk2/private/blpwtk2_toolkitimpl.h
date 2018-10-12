b9acda065dfd19710f67da8c6925e297e3424486
rrajappan1 <rrajappan1@bloomberg.net>
2018-10-12 15:24:12 -0400

    blpwtk2/gpu-log

--- src/blpwtk2/private/blpwtk2_toolkitimpl.h
+++ src/blpwtk2/private/blpwtk2_toolkitimpl.h
@@ -52,6 +52,8 @@ class BrowserMainRunner;
 class ProcessHostImpl;
 class Profile;
 class StringRef;
+class GpuDataLogger;
+
 
                         // =================
                         // class ToolkitImpl
@@ -86,6 +88,13 @@ class ToolkitImpl : public Toolkit {
         // call" restriction placed by the browser code so the renderer can
         // freely make sync calls.
 
+    std::unordered_map<int, std::unique_ptr<EmbedderHeapTracerShim>> d_heapTracers;
+        // Registered heap tracers.
+
+    scoped_refptr<GpuDataLogger> d_gpuDataLogger;
+        // GPU data manager observer to log the gpu process messages
+        // note: verbosity of the log depends on chromium gpu debugging switches
+
     ~ToolkitImpl() override;
         // Shutdown all threads and delete the toolkit.  To ensure the same
         // allocator that was used to create the instance is also used to
@@ -102,6 +111,11 @@ class ToolkitImpl : public Toolkit {
         // toolkit is operating as the browser, it launches a
         // BrowerMainRunner.  The browser main runner requires 'sandboxInfo'
         // during its initialization.
+
+    void attachGPUDataLogObserver();
+        // attach the GPU Data logger to the GPU Data manager as observer
+    void detachGPUDataLogObserver();
+        // remove the GPU Data logger attached the GPU Data manager as observer
 
     std::string createProcessHost(
             const sandbox::SandboxInterfaceInfo& sandboxInfo,
