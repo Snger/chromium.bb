317e7725b3a1255ae0dbc4ae9967dc4e3105fa5e
Imran Haider <ihaider3@bloomberg.net>
2018-10-28 20:18:49 -0400

    Update devkit version to bb1102

--- src/build/make_devkit.py
+++ src/build/make_devkit.py
@@ -187,7 +187,7 @@ def addGenFiles():
 
 
 def main(args):
-  version = "bb1101"   # SET BB VERSION NUMBER HERE
+  version = "bb1102"   # SET BB VERSION NUMBER HERE
 
   outDir = None
   doClean = False
