a6a3cb1509380cdd5267c12af661e6b7d2258727
Alex Betts <abetts3@bloomberg.net>
2018-11-08 18:07:58 -0500

    Update devkit version bb1105

--- src/build/make_devkit.py
+++ src/build/make_devkit.py
@@ -183,7 +183,7 @@ def addGenFiles():
 
 
 def main(args):
-  version = "bb1104"   # SET BB VERSION NUMBER HERE
+  version = "bb1105"   # SET BB VERSION NUMBER HERE
 
   outDir = None
   doClean = False
