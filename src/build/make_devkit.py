1ba915caebc09ee9db05cbfdc3fc93a05a3d82f5
Inseob Lee <ilee56@bloomberg.net>
2018-12-14 14:52:05 -0500

    Update devkit version to bb1203

--- src/build/make_devkit.py
+++ src/build/make_devkit.py
@@ -183,7 +183,7 @@ def addGenFiles():
 
 
 def main(args):
-  version = "bb1202"   # SET BB VERSION NUMBER HERE
+  version = "bb1203"   # SET BB VERSION NUMBER HERE
 
   outDir = None
   doClean = False
