b8e405b71c2193da8a8c18a5c82cdf74abec6193
Alex Betts <abetts3@bloomberg.net>
2018-12-21 12:48:51 -0500

    Update devkit version to 1301

--- src/build/make_devkit.py
+++ src/build/make_devkit.py
@@ -183,7 +183,7 @@ def addGenFiles():
 
 
 def main(args):
-  version = "bb1207"   # SET BB VERSION NUMBER HERE
+  version = "bb1301"   # SET BB VERSION NUMBER HERE
 
   outDir = None
   doClean = False
